7c056545e892e3919b18f586e6854b40cae0f6bf
amin2ace <amin.avmap3240@gmail.com>
2023-12-01 16:22:31 +0330

    add some pep8 in directory_Tree

--- SysProjects/Codes/SelfModules/directory_Tree.py
+++ SysProjects/Codes/SelfModules/directory_Tree.py
@@ -1,18 +1,31 @@
-r"""_summary_:
-            This module represent a class named 'Tree' witch generates
-            directory tree of all subfolders for specific path.
-            representation description:
-                (+) represent directories witch still have sibling directories.
-                (.)present the last directory or last file
-            to save result in a text file use 'to_file' method after instanciating.
-            ex: Tree('C:/Users')
-
-            author: 'Amin Vahdani'
-            feedback: avmap.py@gmail.com
 """
+
+directory_tree.py
+
+This module represent a class 'Tree'
+witch generates directory tree of all
+subfolders for specific path.
+
+representation description: 
+    (+) represent directories witch
+        still have sibling directories.
+
+    (.)present the last directory or 
+        last file
+to save result in a text file use 'to_file'
+method after instanciating.
+
+ex: Tree('C:/Users')
+
 __author__ = 'Amin Vahdani'
-__email__  = 'avmap.py@gmail.com'
+__email__ = 'avmap.py@gmail.com'
 __check__ = ['black', 'isort', 'mypy']
+
+"""
+
+__author__ = "Amin Vahdani"
+__email__ = "avmap.py@gmail.com"
+__check__ = ["black", "isort", "mypy"]
 
 
 import os
@@ -24,7 +37,7 @@ class Tree:
 
     __repr__:
         tree-ish directory in (str)
-    
+
     to_file() method:
         save presentation of tree_ish in a text file in current directory
         argument:
@@ -33,33 +46,30 @@ class Tree:
                 str
     """
 
-    __IND:int = 0 # indention for sub-directories in tree formatting
-    __write_list:list = [] # tree-ish to be presented
-
+    __IND: int = 0  # indention for sub-directories in tree formatting
+    __write_list: list = []  # tree-ish to be presented
 
     def __init__(self, root: str = os.getcwd()) -> None:
-        self.root: str = root.replace('\\','/')
-
+        self.root: str = root.replace("\\", "/")
 
     def to_file(self, file_name: str):
         """save the object to a text file in same current directory with (.txt) extension
 
         Args:
             file_name (str): file name to be save
-        """        
-        file_name = f'{file_name}.txt'
-        with open (os.path.join(self.root, file_name), 'w', encoding='utf-8') as f:
-            for line in self.tree_ish(self.root):# pass the path of directory
-                f.write(f'{line}\n')
+        """
+        file_name = f"{file_name}.txt"
 
- 
-    def tree_ish(self, __dir: str):
-        #Generates object's tree with walk through the directories   
+        with open(os.path.join(self.root, file_name), "w", encoding="utf-8") as f:
+            for line in self.tree_ish(self.root):  # pass the path of directory
+                f.write(f"{line}\n")
 
-        for _,directories,files in os.walk(__dir):
-            for file in files :
-                # prints last file Terminator '.└──' if its last file in the branch 
-                if file is files[-1] and len(directories) is 0: 
+    # Generates object's tree with walk through the directories
+    def __tree_ish(self, __dir: str):
+        for _, directories, files in os.walk(__dir):
+            for file in files:
+                # prints last file Terminator '.└──' if its last file in the branch
+                if file is files[-1] and len(directories) == 0:
                     Tree.__write_list.append(f"{' '*Tree.__IND}.└──{file}")
                 else:
                     Tree.__write_list.append(f"{' '*Tree.__IND} ├──{file}")
@@ -70,13 +80,15 @@ class Tree:
                     Tree.__write_list.append(f"{' '*Tree.__IND}+├──{directory_}")
                 else:
                     Tree.__write_list.append(f"{' '*Tree.__IND}.└──{directory_}")
-                Tree.__IND += 5 # indent for subdirs and files
-                self.tree_ish(os.path.join(__dir, directory_))
-            Tree.__IND -= 5 # reset indent for directories in the same parent 
+                Tree.__IND += 5  # indent for subdirs and files
+                self.__tree_ish(os.path.join(__dir, directory_))
+
+            Tree.__IND -= 5  # reset indent for directories in the same parent
+
             return Tree.__write_list
-        
 
     def __repr__(self) -> str:
         for line in self.tree_ish(self.root):
             print(line)
-        return ''
+
+        return ""
